772019e554e3a13ee40cdba605a0d97c26542359
zxcfdcmv <zxcfdcmv@foxmail.com>
2025-12-28 20:14:27 +0800

    2

--- scripts/link-sbxlm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-srcDir="$1"      # /nix/store/xxx-sbxlm
-linkDir="$2"     # $HOME/.local/share/fcitx5/rime
-mkdir -p "$linkDir"
-find "$srcDir" -mindepth 1 -maxdepth 1 -exec ln -sfn {} "$linkDir/" \;
