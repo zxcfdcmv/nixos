7b5a3bdb3ace5e79742a178802b48ece009e7f1f
zxcfdcmv <zxcfdcmv@foxmail.com>
2026-01-08 23:35:02 +0800

    优化代理配置

--- modules/nixos/proxy.nix
+++ modules/nixos/proxy.nix
@@ -8,13 +8,14 @@
         lan_interface: auto
         wan_interface: auto
         log_level: info
+        allow_insecure: true
       }
 
       subscription {
-        'https://gh-proxy.com/raw.githubusercontent.com/chengaopan/AutoMergePublicNodes/master/list.meta.yml'
-        'https://proxy.v2gh.com/https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub'
-        'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta.yaml'
-        'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta-2.yaml'
+        # 'https://gh-proxy.com/raw.githubusercontent.com/chengaopan/AutoMergePublicNodes/master/list.meta.yml'
+        # 'https://proxy.v2gh.com/https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub'
+        # 'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta.yaml'
+        # 'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta-2.yaml'
         'https://gh-proxy.com/raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt'
       }
 
@@ -25,6 +26,11 @@
       }
 
       routing {
+        domain(keyword:gh-proxy) -> direct
+        domain(keyword:steamcommunity) -> proxy
+        domain(keyword:steampowered) -> proxy
+        domain(keyword:steamstatic) -> direct
+
         dip(geoip:private) -> direct
         dip(geoip:cn) -> direct
         fallback: proxy
