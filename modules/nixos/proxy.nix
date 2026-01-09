101df98ce7db257d6a637b839eda2a5e7ce97d1d
zxcfdcmv <zxcfdcmv@foxmail.com>
2026-01-09 20:02:40 +0800

    优化代理配置

--- modules/nixos/proxy.nix
+++ modules/nixos/proxy.nix
@@ -7,7 +7,9 @@
       global {
         lan_interface: auto
         wan_interface: auto
-        log_level: info
+        log_level: warn
+        dial_mode: ip
+        check_interval: 300s
         allow_insecure: true
       }
 
@@ -19,6 +21,22 @@
         'https://gh-proxy.com/raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt'
       }
 
+      dns {
+        upstream {
+          alidns: 'udp://223.5.5.5:53'
+          tencentdns: 'udp://119.29.29.29:53'
+        }
+        routing {
+          request {
+            qname(geosite:cn) -> alidns
+            fallback: tencentdns
+          }
+          response {
+            fallback: accept
+          }
+        }
+      }
+
       group {
         proxy {
           policy: min_moving_avg
@@ -26,13 +44,16 @@
       }
 
       routing {
+        pname(NetworkManager, systemd, dhclient) -> direct
         domain(keyword:gh-proxy) -> direct
-        domain(keyword:steamcommunity) -> proxy
-        domain(keyword:steampowered) -> proxy
+
+        domain(keyword:steamcommunity,steampowered) -> proxy
         domain(keyword:steamstatic) -> direct
 
+        domain(geosite:cn) -> direct
         dip(geoip:private) -> direct
         dip(geoip:cn) -> direct
+
         fallback: proxy
       }
     '';
