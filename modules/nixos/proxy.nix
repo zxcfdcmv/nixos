0228c2b4e3ffd01bfc9ed2a4bb60d67927f989ba
zxcfdcmv <zxcfdcmv@foxmail.com>
2026-02-13 13:32:56 +0800

    修改镜像

--- modules/nixos/proxy.nix
+++ modules/nixos/proxy.nix
@@ -17,7 +17,8 @@
         # 'https://proxy.v2gh.com/https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub'
         # 'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta.yaml'
         # 'https://gh-proxy.com/raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta-2.yaml'
-        'https://gh-proxy.com/raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt'
+        # 'https://ghfast.top/https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt'
+        'https://gh-proxy.com/raw.githubusercontent.com/chengaopan/AutoMergePublicNodes/master/list.txt'
       }
 
       dns {
